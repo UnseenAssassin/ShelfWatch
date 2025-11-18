56838518d91bbcd6313a79b4bf67e0c9360381c0
Zuriel-Pagan <zurielpagan@gmail.com>
2025-11-17 21:35:51 -0500

    Start of CDK stuff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ShelfWatch
 This is a project for Cloud Computing fall 25.  ShelfWatch is an application where users can input their pantry/grocery items and the app will track when the item will expire.  The user will receive a notification when the food is about to go bad or when it has expired.
+
+
+Services Used: 
+EC2: 
+S3:
+DynamoDB:
+Lambda:
+Amazon SNS:
+IAM:
+Amazon Cognito:  
--- Shelf-Watch/lib/shelf-watch-stack.js
+++ Shelf-Watch/lib/shelf-watch-stack.js
@@ -1,6 +1,16 @@
 const { Stack, Duration } = require('aws-cdk-lib/core');
 // const sqs = require('aws-cdk-lib/aws-sqs');
 
+const { Stack, RemovalPolicy } = require('aws-cdk-lib');
+const dynamodb = require('aws-cdk-lib/aws-dynamodb');
+const S3 = require('aws-cdk-lib/aws-s3');
+const ec2 = require('aws-cdk-lib/aws-ec2');
+const iam = require('aws-cdk-lib/aws-iam');
+const lambda = require('aws-cdk-lib/aws-lambda');
+const sns = require('aws-cdk-lib/aws-sns');
+const cognito = require('aws-cdk-lib/aws-cognito');
+
+
 class ShelfWatchStack extends Stack {
   /**
    *
@@ -8,15 +18,19 @@ class ShelfWatchStack extends Stack {
    * @param {string} id
    * @param {StackProps=} props
    */
-  constructor(scope, id, props) {
+  constructor(scope, id, props) 
+  {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ShelfWatchQueue', {
-    //   visibilityTimeout: Duration.seconds(300)
-    // });
+//===============================================================================
+// DynamoDB Table
+//============================================================================
+    const table = new dynamodb.Table(this, 'ShelfWatchTable',
+      {
+      partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
+      removalPolicy: removalPolicy.DESTROY
+      
+    })
+   
   }
 }
 
